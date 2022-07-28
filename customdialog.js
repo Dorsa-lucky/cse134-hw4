a344c018155695bb91f17fbf4198a9ebc25b1252
Dorsa-lucky <dtayebip@ucsd.edu>
2022-07-27 18:06:16 -0700

    change

--- customdialog.js
+++ customdialog.js
@@ -38,21 +38,45 @@ export function displayConfirm(){
 document.getElementById("confirmTrigger").addEventListener("click", displayConfirm);
 
 //confirming OK
-
 export function confirmOk(){
     enableBtns();
     document.getElementById("dialogConfirm").close();
-    let confirmButton = document.getElementById("result");
-    confirmButton.textContent = `The value returned by the confirmation method is: True`;
-    //result.innerText = "The value returned by the confirmation method is: True";
+    result.innerText = "The value returned by the confirmation method is: True";
 }
 document.getElementById("confirmOK").addEventListener("click", confirmOk);
 
-
+//confirming Cancel
 export function confirmCancel(){
     enableBtns();
     document.getElementById("dialogConfirm").close();
-    confirmButton = document.getElementById("result");
-    confirmButton.textContent = `The value returned by the confirmation method is: False`;
+    result.innerText = "The value returned by the confirmation method is: False";
+}
+document.getElementById("confirmCancel").addEventListener("click", confirmCancel);
+
+//displaying prompt
+export function displayPrompt(){
+    disableBtns();
+    document.getElementById("dialogPrompt").show();
+}
+document.getElementById("promptMe").addEventListener("click", displayPrompt);
+
+//prompting OK
+export function confirmOk(){
+    enableBtns();
+    document.getElementById("dialogPrompt").close();
+    let userAdjective = document.getElementById("adjective").value;
+    if(userAdjective != null && userAdjective != ""){
+        result.innerHTML = `The value returned by the prompt method is: ${userAdjective}`;
+      }else{
+        result.innerHTML = "User didn’t enter anything!";
+      } 
+}
+document.getElementById("confirmOK").addEventListener("click", confirmOk);
+
+//prompting Cancel
+export function confirmCancel(){
+    enableBtns();
+    document.getElementById("dialogPrompt").close();
+    result.innerText = "The value returned by the confirmation method is: False";
 }
 document.getElementById("confirmCancel").addEventListener("click", confirmCancel);
